1f9f81be203c38a06357e359d4e47d7a50838098
JoYongWon <joyw93@naver.com>
2022-06-29 19:50:21 +0900

    debug: next css 렌더링 디버그

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -1,22 +1,41 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static getInitialProps ({ renderPage }) {
-    // Returns an object like: { html, head, errorHtml, chunks, styles }     
-    return renderPage();
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () => originalRenderPage({
+        enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+      });
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } catch (error) {
+      console.error(error);
+    } finally {
+      sheet.seal();
+    }
   }
 
-  render () {    
+  render() {
     return (
-      <html>
-        <Head>
-          <title>My page</title>
-        </Head>
+      <Html>
+        <Head />
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
-    )
+      </Html>
+    );
   }
 }
