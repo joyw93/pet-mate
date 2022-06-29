dfa066a405d484474f0decf34ec668b0adf2b889
JoYongWon <joyw93@naver.com>
2022-06-29 20:19:02 +0900

    refactor: carouselItem 코드 리팩토링

--- client/components/Main/Carousels/Carousels.js
+++ client/components/Main/Carousels/Carousels.js
@@ -1,15 +1,18 @@
-import { CarouselContainer, CarouselItem } from './styled';
-import Carousel from 'react-material-ui-carousel';
-import { Paper, Button } from '@mui/material';
+import { CarouselContainer, CarouselItem } from "./styled";
+import Carousel from "react-material-ui-carousel";
+import { Paper, Button } from "@mui/material";
 const Carousels = () => {
+  const imageUrls = [
+    "img/mainbanner1.png",
+    "img/mainbanner2.png",
+    "img/mainbanner3.png",
+    "img/mainbanner4.png",
+  ];
   return (
     <>
       <CarouselContainer>
-        <Carousel animation='slide' duration={800} interval={2000}>
-          <CarouselItem src='img/mainbanner1.png' />
-          <CarouselItem src='img/mainbanner2.png' />
-          <CarouselItem src='img/mainbanner3.png' />
-          <CarouselItem src='img/mainbanner4.png' />
+        <Carousel animation="slide" duration={800} interval={2000}>
+         {imageUrls.map((imageUrl) => <CarouselItem src={imageUrl}/>)}
         </Carousel>
       </CarouselContainer>
     </>
