92051627ca1887deab84896a0f1d2bbe4c5d41bc
JoYongWon <joyw93@naver.com>
2022-06-29 19:41:04 +0900

    debug: next css 렌더링 디버그

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -1,37 +1,22 @@
-import Document, { Head, Main, NextScript } from "next/document";
-// Import styled components ServerStyleSheet
-import { ServerStyleSheet } from "styled-components";
+import Document, { Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    // Step 1: Create an instance of ServerStyleSheet
-    const sheet = new ServerStyleSheet();
-
-    // Step 2: Retrieve styles from components in the page
-    const page = renderPage(
-      (App) => (props) => sheet.collectStyles(<App {...props} />)
-    );
-
-    // Step 3: Extract the styles as <style> tags
-    const styleTags = sheet.getStyleElement();
-
-    // Step 4: Pass styleTags as a prop
-    return { ...page, styleTags };
+  static getInitialProps ({ renderPage }) {
+    // Returns an object like: { html, head, errorHtml, chunks, styles }     
+    return renderPage();
   }
 
-  render() {
+  render () {    
     return (
       <html>
         <Head>
           <title>My page</title>
-          {/* Step 5: Output the styles in the head  */}
-          {this.props.styleTags}
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </html>
-    );
+    )
   }
 }
