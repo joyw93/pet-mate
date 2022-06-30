274ab5ef8b16b2800a208018611e76cd024a0883
YS <mdlife94@gmail.com>
2022-06-30 17:57:02 +0900

    feature:유저 프로필 페이지 추가

---
+++ client/components/MyProfile/MyPosts/styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import { Colors } from "../../../styles/ColorVariable";
+
+export const PostContainer = styled.div`
+  cursor: pointer;
+
+  img {
+    width: 280px;
+    height: 280px;
+    object-fit: cover;
+    margin-bottom: 20px;
+    margin-right: 25px;
+    border-radius: 5px;
+  }
+`;
