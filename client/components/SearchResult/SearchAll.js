0cfb67363ebdb2abba1a81b1ece4a3b50835cae7
YS <mdlife94@gmail.com>
2022-08-29 15:34:51 +0900

    feature:산책메이트 위치검색

--- client/components/SearchResult/SearchAll.js
+++ client/components/SearchResult/SearchAll.js
@@ -34,7 +34,10 @@ const SearchAll = () => {
   const [communityResults, setCommunityResults] = useState([]);
   const [sanchaekResults, setSanchaekResults] = useState([]);
 
-  //디테일 리셋
+  /* 
+  Todo : 디테일 리셋 기능 추가
+  **/
+
   useEffect(() => {
     dispatch(communityActions.loadPostDetailReset());
     dispatch(sanchaekActions.sanchaekLoadPostDetailReset());
@@ -54,7 +57,7 @@ const SearchAll = () => {
       } else {
         setCommunityResults(searchPosts.communityPosts);
       }
-      if (searchPosts.communityPosts.length > 4) {
+      if (searchPosts.sanchaekPosts.length > 4) {
         const result = searchPosts.sanchaekPosts.slice(0, 4);
         setSanchaekResults(result);
         console.log("산책", result);
@@ -99,9 +102,11 @@ const SearchAll = () => {
                       산책메이트
                       <span>({searchPosts.sanchaekPosts.length})</span>
                     </h1>
-                    <MoreButton onClick={goToSanchaekSearchResults}>
-                      더보기
-                    </MoreButton>
+                    {searchPosts.sanchaekPosts.length > 4 && (
+                      <MoreButton onClick={goToSanchaekSearchResults}>
+                        더보기
+                      </MoreButton>
+                    )}
                   </TitleWrapper>
                   <Box
                     sx={{
@@ -129,9 +134,11 @@ const SearchAll = () => {
                       커뮤니티
                       <span>({searchPosts.communityPosts.length})</span>
                     </h1>
-                    <MoreButton onClick={goToCommunitySearchResults}>
-                      더보기
-                    </MoreButton>
+                    {searchPosts.communityPosts.length > 2 && (
+                      <MoreButton onClick={goToCommunitySearchResults}>
+                        더보기
+                      </MoreButton>
+                    )}
                   </TitleWrapper>
                   {communityResults && (
                     <CommunityResultsContainer>
